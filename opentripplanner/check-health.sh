5cc13b1c5d8f1f2b49c4f7b7c7c804ec7895ef5d
Daniel Langbein <daniel@systemli.org>
2023-03-30 14:37:26 +0200

    feat: improved healthcheck error message

--- opentripplanner/check-health.sh
+++ opentripplanner/check-health.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 set -u
 
-# -sS: disable progress meter but still show error messages
-# -f: Fail fast with no output at all on server errors
-curl -sSf --request POST --url 'http://localhost:8080/otp/routers/default/index/graphql' --header 'Content-Type: application/json' --header 'OTPTimeout: 1000' --data '{"query":"query agencies {agencies {name}}","operationName":"agencies"}' | grep --fixed-strings "\"name\":\"${HEALTHCHECK_AGENCY_NAME}\"" || exit 1
+# `curl` options
+#   -sS: disable progress meter but still show error messages
+#   -f: Fail fast with no output at all on server errors
+
+response="$(curl -sSf --request POST --url 'http://localhost:8080/otp/routers/default/index/graphql' --header 'Content-Type: application/json' --header 'OTPTimeout: 1000' --data '{"query":"query agencies {agencies {name}}","operationName":"agencies"}')" || exit 1
+printf '%s' "${response}" | grep --fixed-strings "\"name\":\"${HEALTHCHECK_AGENCY_NAME}\"" || {
+  printf '%s\n%s\n' "Unexpected response:" "${response}"
+  exit 1
+}
--- pelias/check-health.sh
+++ pelias/check-health.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 set -u
 
-# curl
+# `curl` options
 #   -sS: disable progress meter but still show error messages
 #   -f: Fail fast with no output at all on server errors
-curl -sSf "http://${HEALTHCHECK_REQUEST}" | grep --fixed-strings "${HEALTHCHECK_RESPONSE}" || exit 1
 
-# For debugging purposes, don't filter/check the output:
-#curl -sSf "http://${HEALTHCHECK_REQUEST}" || exit 1
+response="$(curl -sSf "http://${HEALTHCHECK_REQUEST}")" || exit 1
+printf '%s' "${response}" | grep --fixed-strings "${HEALTHCHECK_RESPONSE}" || {
+  printf '%s\n%s\n' "Unexpected response:" "${response}"
+  exit 1
+}
